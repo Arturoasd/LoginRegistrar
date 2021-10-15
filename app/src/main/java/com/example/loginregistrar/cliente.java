9254110c3f2b1c24b418fe5f69e46d322fbc2ba0
arturo <hernan.silva18@inacapmail.cl>
2021-10-15 19:21:58 -0300

    commit maximo v2

---
+++ app/src/main/java/com/example/loginregistrar/cliente.java
@@ -0,0 +1,14 @@
+package com.example.loginregistrar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vista_cliente);
+    }
+}
---
+++ app/src/main/java/com/example/loginregistrar/editar_trabajador.java
@@ -0,0 +1,14 @@
+package com.example.loginregistrar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editar_trabajador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vista_editar_trabajador);
+    }
+}
